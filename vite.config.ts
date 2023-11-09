413a49330b98b158ae8ccf90fc205eca4b4cb24e
Nathan Babb <nbabb@avantia-inc.com>
2023-11-09 13:22:11 -0500

    Fix relative asset path

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [react()],
     test: {
         globals: true,
         environment: 'jsdom',
         setupFiles: ['./src/test/setup.ts'],
     },
+    build: {
+        outDir: 'docs'
+    }
 });
